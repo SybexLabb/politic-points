fef61394d68a5f7e51c68da224134aa9666cd6b3
anus2201a <anus2201a@email.com>
2024-04-16 18:53:22 +0500

    chart changes

--- political_point (6).sql
+++ political_point (6).sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Apr 01, 2024 at 06:03 PM
+-- Generation Time: Apr 02, 2024 at 09:28 AM
 -- Server version: 10.4.24-MariaDB
 -- PHP Version: 7.4.29
 
